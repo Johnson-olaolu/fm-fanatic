086aa7e4b92c06789fe4122b4c061abbbb0559ec
kastroud <johnsonolaolu@gmail.com>
2020-09-14 00:26:10 +0100

    learning GitHub

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './components/App'
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import $ from 'jquery'
+import Popper from 'popper.js'
+import './components/app.css'
+ReactDom.render(<App/>, document.getElementById("App"))
